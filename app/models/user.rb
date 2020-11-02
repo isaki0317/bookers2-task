38950ea4cb1a26f7bd045b75632ed467e21a1d66
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-02 03:47:25 +0000

    update follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+  
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
-  validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member 
+  end  
+  
   resources :books do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
